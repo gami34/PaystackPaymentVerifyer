823bf7ddcf759695d09e07ca47a0ac68b1726cfb
Gideon Mmeremnwanne <gmmeremnwanne@gmail.com>
2021-10-14 11:26:08 +0100

    connect to mongodb cloud service

--- src/config/components/database.config.js
+++ src/config/components/database.config.js
@@ -1,27 +1,29 @@
-"use strict";
+'use strict'
 
-const joi = require("joi");
+const joi = require('joi')
+require('dotenv').config()
 
 /**
  * Generate a validation schema using joi to check the type of your environment variables
  */
 const envSchema = joi
   .object({
-    DB_USER: joi.string().optional().empty(""),
+    DB_USER: joi.string().optional().empty(''),
     DB_HOST: joi.string(),
-    DB_PASSWORD: joi.string().optional().empty(""),
+    DB_PASSWORD: joi.string().optional().empty(''),
     DB_DATABASE: joi.string(),
     DB_PORT: joi.string(),
+    DB_CLOUD: joi.string()
   })
   .unknown()
-  .required();
+  .required()
 
 /**
  * Validate the env variables using joi.validate()
  */
-const { error, value: envVars } = envSchema.validate(process.env);
+const { error, value: envVars } = envSchema.validate(process.env)
 if (error) {
-  throw new Error(`Config validation error: ${error.message}`);
+  throw new Error(`Config validation error: ${error.message}`)
 }
 
 const config = {
@@ -30,8 +32,9 @@ const config = {
     host: envVars.DB_HOST,
     password: envVars.DB_PASSWORD,
     database: envVars.DB_DATABASE,
-    port: envVars.DB_PORT,
-  },
-};
+    db_cloud: envVars.DB_CLOUD,
+    port: envVars.DB_PORT
+  }
+}
 
-module.exports = config;
+module.exports = config
--- src/connectors/mongodb.connector.js
+++ src/connectors/mongodb.connector.js
@@ -1,28 +1,26 @@
-const config = require("../config");
-const mongoose = require("mongoose");
+const config = require('../config')
+const mongoose = require('mongoose')
 
 module.exports = async function () {
-  // set locals, only providing error in development mode
-  const { database, port, host } = config.databaseConfig;
-  const { isDevelopment } = config;
+  const { isDevelopment } = config
   mongoose
     .connect(
-      `mongodb://${host}:${port}/${database}?retryWrites=true&w=majority`,
+      config.databaseConfig.db_cloud,
       {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
+        useUnifiedTopology: true
+      }
     )
     .then(() => {
       if (isDevelopment) {
-        console.log("connected to mongodb");
+        console.log('connected to mongodb')
       }
     })
     .catch((error) => {
       if (isDevelopment) {
-        console.error("Error connected to mongodb:", error.reason);
+        console.error('Error connected to mongodb:', error.reason)
       }
 
-      process.exit(1);
-    });
-};
+      process.exit(1)
+    })
+}
